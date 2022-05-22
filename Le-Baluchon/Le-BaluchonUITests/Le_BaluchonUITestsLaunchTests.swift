e37f7647668770bf503cd865c4207b560d704c30
zouritre <timouf97400@live.fr>
2022-05-22 19:59:55 +0200

    Base project structure

---
+++ Le-Baluchon/Le-BaluchonUITests/Le_BaluchonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Le_BaluchonUITestsLaunchTests.swift
+//  Le-BaluchonUITests
+//
+//  Created by Bertrand Dalleau on 22/05/2022.
+//
+
+import XCTest
+
+class Le_BaluchonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
