2c727729e7effe4bf7003670b19294f4cbe45f15
zouritre <timouf97400@live.fr>
2022-05-22 22:20:25 +0200

    Added tab bar

--- Le-Baluchon/Le-Baluchon/Controller/ExchangeRateViewController.swift
+++ Le-Baluchon/Le-Baluchon/Controller/ExchangeRateViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExchangeRateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Le-Baluchon/Le-Baluchon/Controller/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherViewController.swift
+//  Le-Baluchon
+//
+//  Created by Bertrand Dalleau on 22/05/2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
