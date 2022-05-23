f3dea8795dcc3cc7abba5d17fd0695ed9dbdb58c
zouritre <timouf97400@live.fr>
2022-05-23 02:11:33 +0200

    Set up UI for Exchange conversion Tab

--- Le-Baluchon/Le-BaluchonTests/Le_BaluchonTests.swift
+++ Le-Baluchon/Le-BaluchonTests/Le_BaluchonTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Le_Baluchon
 
-class Le_BaluchonTests: XCTestCase {
+class GetExchangeRateTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,7 +18,7 @@ class Le_BaluchonTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func given() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
