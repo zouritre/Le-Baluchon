b13e32c6d89ead483249c53b3af637df141a1ffa
zouritre <timouf97400@live.fr>
2022-06-14 16:07:25 +0200

    Changed UI colors style

--- Le-Baluchon/Le-Baluchon/Controller/ViewControllerExtension.swift
+++ Le-Baluchon/Le-Baluchon/Controller/ViewControllerExtension.swift
@@ -11,9 +11,9 @@ import UIKit
 extension UIViewController {
     
     func setGradientBackground() {
-        let colorTop =  UIColor(red: 0/255.0, green: 149.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
-        let colorBottom = UIColor(red: 0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
-                    
+        let colorTop =  UIColor(red: 105.0/255.0, green: 103.0/255.0, blue: 115.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 239.0/255.0, green: 241.0/255.0, blue: 243.0/255.0, alpha: 1.0).cgColor
+
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop, colorBottom]
         gradientLayer.locations = [0.0, 1.0]
